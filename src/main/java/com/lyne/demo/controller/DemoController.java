896b2d38e25a68710fa8c20cacda737cbdd40424
hidewnd <908751915@qq.com>
2023-03-06 21:46:51 +0800

    add ad

---
+++ src/main/java/com/lyne/demo/controller/DemoController.java
@@ -0,0 +1,19 @@
+package com.lyne.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author lyne
+ * @date 2023/3/6
+ */
+@RestController
+@RequestMapping("/demo")
+public class DemoController {
+
+	@GetMapping("/test")
+	public String test() {
+		return "123123";
+	}
+}
